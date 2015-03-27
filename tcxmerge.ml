f894d1e42ce2b1efc566b54a74178bb5ce4fbc7b
Anton Yabchinskiy <arn@bestmx.ru>
2015-03-27 18:51:09 +0300

    Change command line arg names

--- tcxmerge.ml
+++ tcxmerge.ml
@@ -6,22 +6,23 @@ let error msg =
 (* Command line arguments *)
 
 let parse_args () =
-  let in_path = ref "/dev/fd/0" in
-  let out_path = ref "/dev/fd/1" in
+  let input_path = ref "/dev/fd/0" in
+  let output_path = ref "/dev/fd/1" in
   let track_path = ref "track.gpx" in
-  Arg.parse [ "-in", Arg.Set_string in_path,
-              Printf.sprintf "Path to input TCX file (default \"%s\")" !in_path;
-              "-out", Arg.Set_string out_path,
-              Printf.sprintf "Path to output TCX file (default \"%s\")" !out_path;
+  Arg.parse [ "-input", Arg.Set_string input_path,
+              Printf.sprintf "Path to input TCX file (default \"%s\")" !input_path;
+              "-output", Arg.Set_string output_path,
+              Printf.sprintf "Path to output TCX file (default \"%s\")" !output_path;
               "-track", Arg.Set_string track_path,
               Printf.sprintf "Path to GPX file with GPS data (default \"%s\")" !track_path ]
             (fun _anon -> ())
             "Merge TCX data (heart rate, cadence) with GPS data from a GPX file";
-  !in_path, !out_path, !track_path
+  !input_path, !output_path, !track_path
 
 (* Main *)
 
 let () =
-  let in_path, out_path, track_path = parse_args () in
+  let input_path, output_path, track_path =
+    parse_args () in
   (* TODO *)
   ()
